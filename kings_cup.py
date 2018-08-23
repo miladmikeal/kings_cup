90f425cf99030075b82a7aabac2a6d0bacf9323f
Milad Mikeal <miladmikeal@gmail.com>
2018-08-23 15:26:09 -0700

    Minor updates

--- kings_cup.py
+++ kings_cup.py
@@ -1,50 +1,53 @@
 import random
 
-class Participant(object):
+class Participant:
 	def __init__(self, name, gender):
 		self.name = name
 		self.gender = gender
 		self.cards = []
-	def show_cards(self):
-		print self.name 
-		print self.cards 
+	def drawCard(self):
+		self.cards = deck.pop()
+	def showCards(self):
+		print(self.name) 
+		print(self.cards) 
 
 
-class Deck(object):
+class Card:
 	def __init__(self, value, suite):
 		self.value = value
 		self.suite = suite
-	def getvalue(self):
+	def getValue(self):
 		return self.value
-	def getsuite(self):
+	def getSuite(self):
 		return self.suite
 	def __str__(self):
 		return ("{0} of {1}".format(self.value, self.suite))
 
-def shuffled_deck():
-	deck = []
+deck = []
+participants = []
+
+def shuffleDeck():
 	for suite in ["Clubs", "Diamonds", "Hearts", "Spades"]:
 		for num in ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]:
-			deck.append(Deck(num, suite))
+			deck.append(Card(num, suite))
 	random.shuffle(deck)
 	return deck 
 
 
-players = raw_input("Enter the number of players: ")
-players = int(players)
-if players < 3:
-	print players
-	print type(players)
-	print "Not enough players."
-elif players > 12:
-	print players
-	print type(players)
-	print "That's too many players."
+players = input("Enter the number of players: ")
+numPlayers = int(players)
+if numPlayers < 3:
+	print(players)
+	print("Not enough players.")
+elif numPlayers > 12:
+	print(players)
+	print("That's too many players.")
 else:
-	for i in range(players):
-		i = i + 1
-		participants = {}
-		name = raw_input("Player %s's Name: " %i) 
-		gender = raw_input("Player %s's Gender: " %i)
-		name_gender = name + gender
-		participants.append(name_gender)
+	for player in range(numPlayers):
+		player += 1
+		name = input("Player %s's Name: " %player) 
+		gender = input("Player %s's Gender: " %player)
+		participants.append(Participant(name, gender))
+
+print(participants)
+print(deck)
