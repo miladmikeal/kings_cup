001e2c0848c0166f6ea2e5ed7e9e30acc061d69d
Milad Mikeal <miladmikeal@gmail.com>
2018-08-23 17:09:05 -0700

    Added comments

--- kings_cup.py
+++ kings_cup.py
@@ -1,5 +1,6 @@
 import random
 
+# Participant class implemenation 
 class Participant:
 	def __init__(self, name, gender):
 		self.name = name
@@ -15,7 +16,7 @@ class Participant:
 	def __repr__(self):
 		return self.name
 
-
+# Card class implemenation
 class Card:
 	def __init__(self, value, suite):
 		self.value = value
@@ -29,30 +30,44 @@ class Card:
 	def __repr__(self):
 		return ("{0} of {1}".format(self.value, self.suite))
 
+# Empty deck list
 deck = []
+# Empty participants list
 participants = []
 
+# Function to shuffle deck
 def shuffleDeck():
-	for suite in ["Clubs", "Diamonds", "Hearts", "Spades"]:
+	# Loop through suits
+	for suit in ["Clubs", "Diamonds", "Hearts", "Spades"]:
+		# Loop through values
 		for num in ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]:
-			deck.append(Card(num, suite))
+			# Add Card to deck
+			deck.append(Card(num, suit))
+			# Shuffle deck
 			random.shuffle(deck)
 	return deck 
 
-
+# Get number of players
 players = input("Enter the number of players: ")
+# Convert to integer
 numPlayers = int(players)
+
+# If num players less than 3
 if numPlayers < 3:
 	print(players)
 	print("Not enough players.")
+# If num players more than 12
 elif numPlayers > 12:
 	print(players)
 	print("That's too many players.")
 else:
+	# Loop through number of players
 	for player in range(numPlayers):
 		player += 1
+		# Input name $ gender for each player
 		name = input("Player %s's Name: " %player) 
 		gender = input("Player %s's Gender: " %player)
+		# Add participant to participants list
 		participants.append(Participant(name, gender))
 
 deck = shuffleDeck()
