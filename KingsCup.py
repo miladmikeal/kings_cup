333be36744ad5ea3d112d2764467a829ad09c90e
Milad Mikeal <miladmikeal@gmail.com>
2019-01-31 00:25:09 -0800

    Add doc string

--- KingsCup.py
+++ KingsCup.py
@@ -6,8 +6,9 @@ class KingsCup:
     """Represents the game of Kings Cup. Gets user input and runs the game."""
 
     def __init__(self):
+        """Initializes variables and calls function to get user inputs."""
         self.participants = []
-        self.numPlayers = 0
+        self.num_players = 0
         self.name = "Game of Kings Cup"
         self.get_inputs()
 
@@ -16,21 +17,21 @@ class KingsCup:
 
         players = input("Enter the number of players: ")
 
-        self.numPlayers = int(players)
+        self.num_players = int(players)
 
         # If num players less than 3
-        if self.numPlayers < 3:
+        if self.num_players < 3:
             print(players)
             print("Not enough players.")
 
         # If num players more than 12
-        elif self.numPlayers > 12:
+        elif self.num_players > 12:
             print(players)
             print("That's too many players.")
 
         else:
             # Loop through number of players
-            for player in range(self.numPlayers):
+            for player in range(self.num_players):
                 player += 1
                 # Input name $ gender for each player
                 name = input("Player %s's Name: " % player)
@@ -48,23 +49,23 @@ class KingsCup:
         print()
 
         i = 0
-        numKings = 0
+        num_kings = 0
 
         # Loop until deck is empty
-        while len(deck.get_deck()) != 0 and numKings != 4:
+        while len(deck.get_deck()) != 0 and num_kings != 4:
             input("Press Enter to continue...")
 
             # Call drawCard method for players continuously
-            card = self.participants[i % self.numPlayers].draw_card(deck)
+            card = self.participants[i % self.num_players].draw_card(deck)
             # Pour into kings cup if card is king
 
             if card.get_value() is "King":
                 print("Pour into the Kings Cup!")
-                numKings += 1
+                num_kings += 1
 
             # If 4th King drawn, player must drink Kings Cup
-            if numKings == 4:
-                print("{0}, DRINK THE KINGS CUP!".format(self.participants[i % self.numPlayers].get_name()))
+            if num_kings == 4:
+                print("{0}, DRINK THE KINGS CUP!".format(self.participants[i % self.num_players].get_name()))
 
             i += 1
             print()
